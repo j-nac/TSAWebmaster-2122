d97927a670ead73193a69b16985ea7481a6deeb1
Jakob Nacanaynay <63821647+j-nac@users.noreply.github.com>
2021-12-11 11:51:23 -0800

    Make background image fullscreen

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,11 +1,14 @@
-function toggleNavbar(){
+function toggleNavbar() {
     navElement = $(".navbox:first");
     toggleButton = $("#menu-icon");
 
-    if (navElement.css("display") == "none") {
+    if (navElement.css("display") == "none")
+    {
         navElement.css("display", "flex");
         toggleButton.text("close");
-    } else {
+    }
+    else
+    {
         navElement.css("display", "none");
         toggleButton.text("menu");
     }
