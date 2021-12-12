39a69aea2b31eeab338d3b73a7dc8052e4830011
Jakob Nacanaynay <63821647+j-nac@users.noreply.github.com>
2021-12-11 22:08:20 -0800

    Format and code cleanup

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -1,21 +1,19 @@
 target = 1655555555555
 
-function countdown ()
-{
-    var timeTill = Math.max(0, parseInt((target-Date.now())/1000))
+function countdown() {
+    var timeTill = Math.max(0, parseInt((target - Date.now()) / 1000))
     var clock = [timeTill]
-    for(let [i,v] of [60,60,24].entries())
-    {
-        clock.push(parseInt(clock[i]/v))
-        clock[i]%=v
+    for (let [i, v] of [60, 60, 24].entries()) {
+        clock.push(parseInt(clock[i] / v))
+        clock[i] %= v
     }
-    for(let i in clock)
-    {
+    for (let i in clock) {
         clock[i] = String(clock[i])
-        while(clock[i].length<2){clock[i]="0"+clock[i]}
+        while (clock[i].length < 2) {
+            clock[i] = "0" + clock[i]
+        }
     }
-    for(let i of document.getElementsByTagName("countdown"))
-    {
+    for (let i of document.getElementsByTagName("countdown")) {
         i.innerHTML = clock.reverse().join(":")
     }
 }
