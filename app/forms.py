972e73e89ade4f1545851b6016d234a3be18c657
Jakob Nacanaynay <63821647+j-nac@users.noreply.github.com>
2021-12-14 19:02:38 -0800

    Remove bloat

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+# I realize now that using flask wtforms was unnecessary. Shut up
+
 class SearchStore(FlaskForm):
     search = StringField('search')
-    submit = SubmitField()
