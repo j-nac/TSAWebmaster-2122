35ba8eb5c54c6286f3ffd38c493d5dd32c9a16c8
Jakob Nacanaynay <63821647+j-nac@users.noreply.github.com>
2021-12-14 18:59:09 -0800

    Create basic tag and search form

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '1488aa6bf6cbe3616527ee3508db8e52'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+csrf = CSRFProtect(app)
 
-from app import views, models, forms
+from app import views
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, url_for, redirect, flash, request
-from app import app, db
+from app import app, db, csrf
 from app.models import Item, Tag
+from app.forms import SearchStore
 
 @app.route('/')
 @app.route('/index')
@@ -17,31 +18,25 @@ def register():
     return render_template('register.html', title='Register')
 
 @app.route('/store')
+@csrf.exempt
 def store():
-    tags = request.args.get('tags')
-    if tags == None:
-        # If no tags, show all
-        query_results = Item.query.all()
-    else:
-        # Handle multiple tags
-        if ',' in tags:
-            tags = tags.split(',')
-        # Handle a single tag
-        else:
-            tags = [tags]
-        query_results = []
-        for tag_name in tags:
-            # Get tag object
-            tags_obj = Tag.query.filter_by(name=tag_name).first()
-            if tags_obj == None:
-                continue
-            # Get items with tag
-            mini_query_results = Item.query.filter(Item.tags.contains(tags_obj))
-            for r in mini_query_results:
-                # Don't include duplicates
-                if r not in query_results:
-                    query_results.append(r)
-    return render_template('store.html', title='Store', query_results=query_results)
+    tags = request.args.getlist('tags')
+    query_results = []
+    for tag_name in tags:
+        # Get tag object
+        tags_obj = Tag.query.filter_by(name=tag_name).first()
+        if tags_obj == None:
+            continue
+        # Get items with tag
+        mini_query_results = Item.query.filter(Item.tags.contains(tags_obj))
+        for r in mini_query_results:
+            # Don't include duplicates
+            if r not in query_results:
+                query_results.append(r)
+
+    form = SearchStore()
+
+    return render_template('store.html', title='Store', query_results=query_results, form=form)
 
 '''
 To filter by tag:
@@ -53,7 +48,7 @@ where t is a database tag object
 @app.route('/items/<int:id>')
 def item(id):
     item = Item.query.get(id)
-    return render_template('store-item.html', title=f'{item.name} | Store' , item=item)
+    return render_template('item.html', title=f'{item.name} | Store' , item=item)
 
 @app.route('/faq')
 def faq():
