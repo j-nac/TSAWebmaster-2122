b680a456e4452db0721f5c827132e4e57f3d4f5a
Jakob Nacanaynay <63821647+j-nac@users.noreply.github.com>
2021-12-15 20:34:08 -0800

    Add deleting and viewing to db-admin.py

--- db-admin.py
+++ db-admin.py
@@ -3,9 +3,19 @@ from app.models import Item, Tag
 
 if __name__ == '__main__':
     while True:
-        command = int(input('What do you want to do?\n1. Add item\n2. Add tag\n3. Quit\n> '))
+        command = int(input('What do you want to do?\n1. Show items\n2. Add item\n3. Delete item\n4. Show tags\n5. Add tag\n6. Delete tag\n7. Quit\n> '))
 
         if command == 1:
+            item_query = Item.query.all()
+            for i in item_query:
+                print(f'Item {i.id} - {i.name}')
+                print(f'Price: {i.price}')
+                print(f'Description: {i.description}')
+                print(f'Tags: {[t.name for t in i.tags]}')
+                print('\n')
+            input('Press ENTER to close')
+            
+        elif command == 2:
             print('Create item')
 
             name = input('Name: ')
@@ -30,8 +40,28 @@ if __name__ == '__main__':
             db.session.add(new_item)
             db.session.commit()
             print('Done')
+        
+        elif command == 3:
+            item_query = Item.query.all()
+            for i in item_query:
+                print(f'{i.id}. {i.name}')
+            item_del = int(input('Item to delete: '))
+            obj_item_del = Item.query.get(item_del)
+            confirm = input(f'Are you sure you want to delete {obj_item_del.name} (y/n): ')
+            if confirm == 'y':
+                db.session.delete(obj_item_del)
+                db.session.commit()
+                print('Done')
+            else:
+                print('Operation cancelled')
+        
+        elif command == 4:
+            tag_query = Tag.query.all()
+            for t in tag_query:
+                print(f'Tag {t.id} - {t.name}')
+            input('Press ENTER to close')
 
-        elif command == 2:
+        elif command == 5:
             print('Create tag')
             name = input('Name: ')
 
@@ -40,6 +70,20 @@ if __name__ == '__main__':
             db.session.add(new_tag)
             db.session.commit()
             print('Done')
+        
+        elif command == 6:
+            tag_query = Tag.query.all()
+            for t in tag_query:
+                print(f'{t.id}. {t.name}')
+            tag_del = int(input('Tag to delete: '))
+            obj_tag_del = Tag.query.get(tag_del)
+            confirm = input(f'Are you sure you want to delete {obj_tag_del.name} (y/n): ')
+            if confirm == 'y':
+                db.session.delete(obj_tag_del)
+                db.session.commit()
+                print('Done')
+            else:
+                print('Operation cancelled')
         
         else:
             print('Goodbye')
