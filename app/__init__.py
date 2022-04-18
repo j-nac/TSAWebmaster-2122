fd4ca13c34a406612179bf7791ae3c66449700bc
Jakob Nacanaynay <jnac8080@gmail.com>
2022-04-18 07:07:57 -0700

    Make it not stupid

--- app/__init__.py
+++ app/__init__.py
@@ -8,17 +8,16 @@ app = Flask(__name__)
 
 # $env:VARIABLE_NAME = 'value'
 
-# import secrets
-# secrets.token_urlsafe(20)
+import secrets
 
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = secrets.token_urlsafe(20)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQL_URI') # 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASSWORD')
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
