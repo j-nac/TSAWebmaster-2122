da6fd0f8609e4e47c64a9b30b98e586179f7fce8
Jakob Nacanaynay <63821647+j-nac@users.noreply.github.com>
2022-04-24 20:52:21 -0700

    Temp debugging

--- app/__init__.py
+++ app/__init__.py
@@ -12,13 +12,15 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') # secrets.token_urlsafe(20)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQL_URI') # 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
+
+print(app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'], app.config['SQLALCHEMY_DATABASE_URI'])
 
 csrf = CSRFProtect(app)
 
