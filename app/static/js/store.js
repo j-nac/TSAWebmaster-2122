c8ee04ea84184a3e2d145a6b5a5a075bb1bc3228
Asher-W <85578685+Asher-W@users.noreply.github.com>
2022-05-11 12:55:13 -0700

    basic fixes

--- app/static/js/store.js
+++ app/static/js/store.js
@@ -4,3 +4,13 @@ function mason(){
 		$grid.masonry('layout')
 	})
 }
+
+Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
+    mason()
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	document.querySelectorAll('button.store-card').forEach(button => {
+		button.onclick = redirect
+	});
+});
