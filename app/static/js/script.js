8c82916414940d052d1ff5e561f4057b4e088e27
Asher-W <85578685+Asher-W@users.noreply.github.com>
2022-05-15 11:39:24 -0700

    compress js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,3 +1,6 @@
+/*
+BASE FILE
+*/
 var cleanup
 if(!cleanup){
     cleanup = ()=>{}
@@ -78,3 +81,80 @@ document.getElementsByClassName('navbox')[0].addEventListener('keyup', (e)=>{
     }
 })
 */
+/*
+HOME FILE
+*/
+var target = 1656658800000
+
+function countdown() {
+    var timeTill = Math.max(0, ((target - Date.now()) / 1000))
+    var clock = [timeTill]
+    for (let [i, v] of [60, 60, 24].entries()) {
+        clock.push(clock[i] / v)
+        clock[i] %= v
+    }
+    $('#day-cd').css('width', (clock[3]/365*100).toString()+'%');
+    $('#hr-cd').css('width', (clock[2]/24*100).toString()+'%');
+    $('#min-cd').css('width', (clock[1]/60*100).toString()+'%');
+    $('#sec-cd').css('width', (clock[0]/60*100).toString()+'%');
+    $('#days').html(parseInt(clock[3]))
+    $('#hrs').html(parseInt(clock[2]))
+    $('#mins').html(parseInt(clock[1]))
+    $('#secs').html(parseInt(clock[0]))
+    
+    // $("countdown").text(clock[3] + "d : " + clock[2] + "h : " + clock[1] + "m : " + clock[0] + "s")
+}
+
+var inter;
+
+function loadhome(){
+    cleanup = ()=>{
+        console.log('sweeping');
+        clearInterval(inter);
+    };
+    inter=setInterval(countdown, 500);
+
+    // https://simpleparallax.com/
+    var image = document.getElementsByClassName('countdown-background');
+    new simpleParallax(image, {
+        delay: .3,
+        scale: 1.5,
+        transition: 'cubic-bezier(0,0,0,1)'
+    });
+}
+
+/* 
+STORE FILE
+*/
+function mason(){
+	var $grid = $('.grid').masonry({'isFitWidth': true});
+	$grid.imagesLoaded().progress( function() {
+		$grid.masonry('layout')
+	})
+}
+
+function loadstore(){
+	Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
+		mason()
+	});
+
+	document.querySelectorAll('button.store-card').forEach(button => {
+		button.onclick = redirect
+	});
+	document.getElementById('searchbox').onsubmit = function(){
+		$.ajax({
+			url:'/store?'+$('#searchbox').serializeArray().map(e=>e['name']+'='+e['value']).join('&'),
+			type: 'STATIC',
+			success: function(data){
+				let group = document.getElementById('page')
+				let newpage = document.createElement('div')
+				newpage.innerHTML = data
+				group.children[0].remove()
+				newpage.id = 'newpage'
+				group.appendChild(newpage)
+				loadstore()
+			}
+		})
+		return false
+	}
+}
